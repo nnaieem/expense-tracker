5de75fb11ad86f12da154d73a2660e2118884d9d
nnaieem <nicholas.naieem@gmail.com>
2020-07-14 20:58:52 -0400

    POST successful

--- expense-tracker/controllers/transactions.js
+++ expense-tracker/controllers/transactions.js
@@ -1,45 +1,58 @@
-const Transaction = require('../models/Transaction');
+const Transaction = require("../models/Transaction");
 
-// @description Get all transactions
-// @route GET /api/v1/transactions
+// @desc    Get all transactions
+// @route   GET /api/v1/transactions
+
 exports.getTransactions = async (req, res, next) => {
-    try {
-      const transactions = await Transaction.find();
-      
-      return res.status(200).json({
-          success: true,
-          count: transactions.length,
-          data: transactions
-      });
-    } catch (error) {
-       return res.status(500).json({
-           success: false,
-           error: 'Server Error'
-       });
-    }
-}
+  try {
+    const transactions = await Transaction.find();
 
-// @description     Add transaction
-// @route           POST /api/v1/transactions
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+};
+
+// @desc    Add transaction
+// @route   POST /api/v1/transactions
+
 exports.addTransaction = async (req, res, next) => {
-    try {
-        const { text, amount } = req.body;
+  try {
+    const { text, amount } = req.body;
 
-        const transaction = await Transaction.create(req.body);
+    const transaction = await Transaction.create(req.body);
 
-        return res.status(201).json({
-          success: true,
-          data: transaction,
-        });
-    } catch (error) {
-        console.log(err);
-        
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error",
+      });
     }
-    
-}
+  }
+};
 
 // @description     Delete transaction
 // @route           DELETE /api/v1/transactions/:id
+
 exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transactions');
 }
--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1/transactions', transactions)
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
